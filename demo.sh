b4657a7bdd312e581d0dea7a37a3967aaa562c65
Mike Hudman <mhudman@ptc.com>
2024-02-12 18:07:53 -0500

    Update to demo.sh to record the REPOHOST for clarity when demoing AZD vs. Github

--- demo.sh
+++ demo.sh
@@ -2,6 +2,7 @@ export TASK_ID=$1
 export NOW=$(date)
 export EDITFILE="README.md"
 export BRANCHNAME="${TASK_ID}-dev-branch"
+export REPOHOST="$(git remote -v | grep fetch | awk '{print $2}')"
 
 echo ${TASK_ID}
 echo ${NOW}
@@ -9,7 +10,7 @@ echo ${EDITFILE}
 git checkout -b ${BRANCHNAME}
 git checkout ${BRANCHNAME}
 git pull
-echo -e "\n${NOW} - Added line to readme.md for ${TASK_ID}">> ${EDITFILE}
+echo -e "\n${NOW} - Added line to ${REPOHOST}/readme.md for ${TASK_ID}" >> ${EDITFILE}
 cat ${EDITFILE}
 git add ./${EDITFILE}
 git status
